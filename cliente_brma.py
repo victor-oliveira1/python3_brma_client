3f688182305a7e75373db10d303f0efb9f644cfa
Victor Oliveira <victor.oliveira@gmx.com>
2017-05-16 08:44:03 -0300

    Add files via upload

---
+++ cliente_brma.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+#Cliente BRMA escrito em python3.
+#TODO: Implementar a criptografia do applet: CryptP.class
+#victor.oliveira@gmx.com
+
+from urllib.request import urlopen
+from urllib.parse import urlencode
+from telnetlib import Telnet
+from time import sleep
+
+server = 'INSERIR ENDEREÇO IP'
+usuario = 'INSERIR USUÁRIO'
+senha = 'INSERIR SENHA'
+
+def login():
+    url = 'http://{}:10001/cgi/loginaux.cgi'.format(server, porta)
+
+    post = {'acao' : 'login',
+            'CRYPT' : '0',
+            'USERNAME' : usuario,
+            'PASSWORD' : senha}
+    post = urlencode(post).encode()
+    req = urlopen(url, post)
+    print('Conectado.')
+
+def keep_alive():
+    while True:
+        print('Autenticando...')
+        tn = Telnet(server, 10002)
+        tn.write(usuario.encode() + b'\n' + senha.encode() + b'\n')
+        print('Autenticado.')
+        print(tn.read_all().decode())
+        tn.close()
+        sleep(480)
+
+login()
+keep_alive()
